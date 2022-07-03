53a0a0823b86301ca817f6e179b1109a06738677
omgwer <63747048+omgwer@users.noreply.github.com>
2022-07-03 21:10:45 +0300

    добавил методы для работы с приложением

---
+++ practice1/test/practice1_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+import '../lib/service/person_list_service.dart';
+import '../lib/service/app_service.dart';
+
+void main() {
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
+
+  // test('add person test', () {
+
+  // });
+}
