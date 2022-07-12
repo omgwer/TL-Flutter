499442d1114e9a6ef2efc9da2e39774d15746915
Alexandr Kislitsyn <63747048+omgwer@users.noreply.github.com>
2022-07-12 10:07:02 +0300

    add tests

--- office3/lib/main.dart
+++ office3/lib/main.dart
@@ -51,9 +51,9 @@ class ButtonWidget extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextButton(onPressed: grow, child: Text('Press to upScale')),
+        TextButton(onPressed: grow, child: Text('Press to upScale'), key: Key('pressUpScale')),
         const Text('Hello world!'),
-        TextButton(onPressed: downScale, child: Text('Press to downScale')),
+        TextButton(onPressed: downScale, child: Text('Press to downScale'),  key: Key('pressDownScale')),
       ],
     );
   }
@@ -67,16 +67,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      theme: ThemeData(       
         primarySwatch: Colors.blue,
       ),
       home: const MyWidget(),
